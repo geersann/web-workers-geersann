a86bfaeac80daf4650bbc69a4526e9be7d51f866
geersann <148795588+geersann@users.noreply.github.com>
2024-08-21 01:54:03 +0000

    add fibonacci script

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-function calculate() {  
-  
+const inputNumber = document.getElementById('inputNumber');
+const resultDiv = document.getElementById('result');
+
+let worker;
+
+function terminateWorker() {
+    if (worker) {
+        worker.terminate();
+        worker = null;
+    }
+}
+
+function calculate() {
+    const number = parseInt(inputNumber.value);
+
+    resultDiv.innerText = 'Calculating...';
+
+    terminateWorker();
+
+    worker = new Worker('worker.js');
+
+    worker.postMessage({ data: number });
+
+    worker.onmessage = function (event) {
+        resultDiv.innerText = `Result: ${event.data}`;
+        terminateWorker();
+    };
 }
